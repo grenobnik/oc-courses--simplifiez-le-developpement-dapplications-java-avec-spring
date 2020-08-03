7fb500479ee1f15c563475d6767256d3ae21961d
Kirill Vankov <kirill.vankov@pc-scol.fr>
2020-08-03 10:50:05 +0200

    load applicationContext into module ticket-batch

--- ticket/ticket-batch/src/main/java/org/example/demo/ticket/batch/Main.java
+++ ticket/ticket-batch/src/main/java/org/example/demo/ticket/batch/Main.java
@@ -3,7 +3,10 @@ package org.example.demo.ticket.batch;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.example.demo.ticket.business.contract.ManagerFactory;
 import org.example.demo.ticket.model.exception.TechnicalException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 /**
@@ -13,9 +16,10 @@ import org.example.demo.ticket.model.exception.TechnicalException;
  */
 public class Main {
 
-    /** Logger pour la classe */
+    /**
+     * Logger pour la classe
+     */
     private static final Log LOGGER = LogFactory.getLog(Main.class);
-
 
     /**
      * The entry point of application.
@@ -24,6 +28,14 @@ public class Main {
      * @throws TechnicalException sur erreur technique
      */
     public static void main(String[] pArgs) throws TechnicalException {
+
+        ApplicationContext vApplicationContext
+                = new ClassPathXmlApplicationContext("classpath:/applicationContext.xml");
+
+        // Il est possible de récupérer un bean dans ce contexte :
+        ManagerFactory vManagerFactory
+                = vApplicationContext.getBean("managerFactory", ManagerFactory.class);
+
         try {
             if (pArgs.length < 1) {
                 throw new TechnicalException("Veuillez préciser le traitement à effectuer !");
