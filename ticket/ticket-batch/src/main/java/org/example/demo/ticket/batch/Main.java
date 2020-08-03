9b11b5885846c504acd8effbfd5cce46fe71fa3c
Kirill Vankov <kirill.vankov@pc-scol.fr>
2020-08-03 13:45:14 +0200

    Spring configuration via Java class

--- ticket/ticket-batch/src/main/java/org/example/demo/ticket/batch/Main.java
+++ ticket/ticket-batch/src/main/java/org/example/demo/ticket/batch/Main.java
@@ -6,6 +6,7 @@ import org.apache.commons.logging.LogFactory;
 import org.example.demo.ticket.business.contract.ManagerFactory;
 import org.example.demo.ticket.model.exception.TechnicalException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
@@ -30,7 +31,7 @@ public class Main {
     public static void main(String[] pArgs) throws TechnicalException {
 
         ApplicationContext vApplicationContext
-                = new ClassPathXmlApplicationContext("classpath:/applicationContext.xml");
+                = new AnnotationConfigApplicationContext(SpringConfiguration.class);
 
         // Il est possible de récupérer un bean dans ce contexte :
         ManagerFactory vManagerFactory
